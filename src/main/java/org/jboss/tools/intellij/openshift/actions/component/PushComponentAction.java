8bf02d12ec6e7e6402937ed693be08c6dd92b7a5
Jeff MAURY <jmaury@redhat.com>
2018-11-08 19:03:04 +0100

    Add Push command
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

---
+++ src/main/java/org/jboss/tools/intellij/openshift/actions/component/PushComponentAction.java
@@ -0,0 +1,38 @@
+package org.jboss.tools.intellij.openshift.actions.component;
+
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jboss.tools.intellij.openshift.actions.application.OdoAction;
+import org.jboss.tools.intellij.openshift.tree.application.ApplicationNode;
+import org.jboss.tools.intellij.openshift.tree.application.ComponentNode;
+import org.jboss.tools.intellij.openshift.tree.application.ProjectNode;
+import org.jboss.tools.intellij.openshift.utils.ExecHelper;
+import org.jboss.tools.intellij.openshift.utils.UIHelper;
+
+import javax.swing.JOptionPane;
+import javax.swing.tree.TreeNode;
+import javax.swing.tree.TreePath;
+import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
+
+public class PushComponentAction extends OdoAction {
+  public PushComponentAction() {
+    super(ComponentNode.class);
+  }
+
+  @Override
+  public void actionPerformed(AnActionEvent anActionEvent, TreePath path, Object selected, String odo) {
+    ComponentNode componentNode = (ComponentNode) selected;
+    ApplicationNode applicationNode = (ApplicationNode) ((TreeNode) selected).getParent();
+    ProjectNode projectNode = (ProjectNode) applicationNode.getParent();
+    CompletableFuture.runAsync(() -> {
+      try {
+        ExecHelper.execute(odo, "project", "set", projectNode.toString());
+        ExecHelper.execute(odo, "app", "set", applicationNode.toString());
+        ExecHelper.execute(odo, "component", "set", componentNode.toString());
+        ExecHelper.executeWithTerminal(odo, "push");
+      } catch (IOException e) {
+        UIHelper.executeInUI(() -> JOptionPane.showMessageDialog(null, "Error: " + e.getLocalizedMessage(), "Push", JOptionPane.ERROR_MESSAGE));
+      }
+    });
+  }
+}

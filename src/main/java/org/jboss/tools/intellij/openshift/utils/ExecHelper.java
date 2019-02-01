90871dfa16c77ae4826e516ba69f7be3f88adfc0
Jeff MAURY <jmaury@redhat.com>
2019-02-01 15:41:03 +0100

    Fix incompatibility with IJ 2018.3
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- src/main/java/org/jboss/tools/intellij/openshift/utils/ExecHelper.java
+++ src/main/java/org/jboss/tools/intellij/openshift/utils/ExecHelper.java
@@ -1,21 +1,15 @@
 package org.jboss.tools.intellij.openshift.utils;
 
-import com.intellij.execution.ExecutionException;
-import com.intellij.execution.configurations.GeneralCommandLine;
-import com.intellij.execution.process.OSProcessHandler;
 import com.intellij.execution.process.ProcessHandler;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.util.SystemInfo;
 import com.jediterm.terminal.ProcessTtyConnector;
-import com.jediterm.terminal.TerminalDisplay;
-import com.jediterm.terminal.TerminalMode;
 import com.jediterm.terminal.TtyConnector;
-import com.jediterm.terminal.model.JediTerminal;
-import com.jediterm.terminal.ui.TerminalPanelListener;
-import com.jediterm.terminal.ui.TerminalSession;
-import com.jediterm.terminal.ui.TerminalWidget;
-import org.apache.commons.exec.*;
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.PumpStreamHandler;
 import org.apache.xmlgraphics.util.WriterOutputStream;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -23,11 +17,13 @@ import org.jetbrains.plugins.terminal.AbstractTerminalRunner;
 import org.jetbrains.plugins.terminal.TerminalOptionsProvider;
 import org.jetbrains.plugins.terminal.TerminalView;
 
-import javax.swing.JComponent;
-import java.awt.Dimension;
+import java.io.FilterInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeUnit;
 
 public class ExecHelper {
   public static String execute(String executable, String... arguments) throws IOException {
@@ -40,18 +36,137 @@ public class ExecHelper {
       return writer.toString();
   }
 
+  private static class RedirectedStream extends FilterInputStream {
+    private boolean emitLF = false;
+
+    private RedirectedStream(InputStream delegate) {
+      super(delegate);
+    }
+
+    @Override
+    public synchronized int read() throws IOException {
+      if (emitLF) {
+        emitLF = false;
+        return '\n';
+      } else {
+        int c = super.read();
+        if (c == '\n') {
+          emitLF = true;
+          c = '\r';
+        }
+        return c;
+      }
+      //return super.read();
+    }
+
+    @Override
+    public synchronized int read(@NotNull byte[] b) throws IOException {
+      return read(b, 0, b.length);
+    }
+
+    @Override
+    public synchronized int read(@NotNull byte[] b, int off, int len) throws IOException {
+      if (b == null) {
+        throw new NullPointerException();
+      } else if (off < 0 || len < 0 || len > b.length - off) {
+        throw new IndexOutOfBoundsException();
+      } else if (len == 0) {
+        return 0;
+      }
+
+      int c = read();
+      if (c == -1) {
+        return -1;
+      }
+      b[off] = (byte)c;
+
+      int i = 1;
+      try {
+        for (; i < len  && available() > 0; i++) {
+          c = read();
+          if (c == -1) {
+            break;
+          }
+          b[off + i] = (byte)c;
+        }
+      } catch (IOException ee) {}
+      return i;
+    }
+  }
+  private static class RedirectedProcess extends Process {
+    private final Process delegate;
+    private final InputStream inputStream;
+    private final InputStream errorStream;
+
+    private RedirectedProcess(Process delegate) {
+      this.delegate = delegate;
+      inputStream = new RedirectedStream(delegate.getInputStream()) {};
+      errorStream = new RedirectedStream(delegate.getErrorStream()) {};
+    }
+
+    @Override
+    public OutputStream getOutputStream() {
+      return delegate.getOutputStream();
+    }
+
+    @Override
+    public InputStream getInputStream() {
+      return inputStream;
+    }
+
+    @Override
+    public InputStream getErrorStream() {
+      return errorStream;
+    }
+
+    @Override
+    public int waitFor() throws InterruptedException {
+      return delegate.waitFor();
+    }
+
+    @Override
+    public boolean waitFor(long timeout, TimeUnit unit) throws InterruptedException {
+      return delegate.waitFor(timeout, unit);
+    }
+
+    @Override
+    public int exitValue() {
+      return delegate.exitValue();
+    }
+
+    @Override
+    public void destroy() {
+      delegate.destroy();
+    }
+
+    @Override
+    public Process destroyForcibly() {
+      return delegate.destroyForcibly();
+    }
+
+    @Override
+    public boolean isAlive() {
+      return delegate.isAlive();
+    }
+  }
+
   public static void executeWithTerminal(String... command) throws IOException {
       try {
-        OSProcessHandler handler = new OSProcessHandler(new GeneralCommandLine(command));
+        Process p = new ProcessBuilder(command).start();
+        if (SystemInfo.isWindows) {
+          p = new RedirectedProcess(p);
+        }
+
+        final Process process = p;
         AbstractTerminalRunner runner = new AbstractTerminalRunner(ProjectManager.getInstance().getDefaultProject()) {
           @Override
           protected Process createProcess(@Nullable String s) {
-            return handler.getProcess();
+            return process;
           }
 
           @Override
           protected ProcessHandler createProcessHandler(Process process) {
-            return handler;
+            return null;
           }
 
           @Override
@@ -82,63 +197,20 @@ public class ExecHelper {
           public String runningTargetName() {
             return null;
           }
-
-          @Override
-          public void openSession(@NotNull TerminalWidget terminal) {
-            super.openSession(new TerminalWidget() {
-              @Override
-              public TerminalSession createTerminalSession(TtyConnector ttyConnector) {
-                TerminalSession session = terminal.createTerminalSession(ttyConnector);
-                if (session.getTerminal() instanceof JediTerminal) {
-                  ((JediTerminal)session.getTerminal()).setModeEnabled(TerminalMode.AutoNewLine, true);
-                }
-                return session;
-              }
-
-              @Override
-              public JComponent getComponent() {
-                return terminal.getComponent();
-              }
-
-              @Override
-              public boolean canOpenSession() {
-                return terminal.canOpenSession();
-              }
-
-              @Override
-              public void setTerminalPanelListener(TerminalPanelListener terminalPanelListener) {
-                terminal.setTerminalPanelListener(terminalPanelListener);
-              }
-
-              @Override
-              public Dimension getPreferredSize() {
-                return terminal.getPreferredSize();
-              }
-
-              @Override
-              public TerminalSession getCurrentSession() {
-                return terminal.getCurrentSession();
-              }
-
-              @Override
-              public TerminalDisplay getTerminalDisplay() {
-                return terminal.getTerminalDisplay();
-              }
-            });
-          }
         };
         TerminalOptionsProvider terminalOptions = ServiceManager.getService(TerminalOptionsProvider.class);
         boolean previousAutoClassClose = terminalOptions.closeSessionOnLogout();
         terminalOptions.setCloseSessionOnLogout(false);
         ApplicationManager.getApplication().invokeLater(() -> TerminalView.getInstance(ProjectManager.getInstance().getOpenProjects()[0]).createNewSession(ProjectManager.getInstance().getOpenProjects()[0], runner));
-        handler.startNotify();
-        handler.waitFor();
         terminalOptions.setCloseSessionOnLogout(previousAutoClassClose);
-        if (handler.getProcess().exitValue() != 0) {
-          throw new IOException("Process returned exit code: " + handler.getProcess().exitValue(), null);
+        if (p.waitFor() != 0) {
+          throw new IOException("Process returned exit code: " + p.exitValue(), null);
         }
-    } catch (ExecutionException e) {
-        throw new IOException(e.getLocalizedMessage(), e);
+    } catch (IOException e) {
+        throw e;
+      }
+      catch (InterruptedException e) {
+        throw new IOException(e);
       }
   }
 }

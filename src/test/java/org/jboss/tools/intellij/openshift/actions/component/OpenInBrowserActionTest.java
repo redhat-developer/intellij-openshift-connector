e9f7c63530237f65b5dcca04dd0958118fc8e7bb
Jeff MAURY <jeffmaury.github@jeffmaury.com>
2019-04-19 08:05:24 +0200

    Implement Open In Browser action on URLs (#54)
    Fixes #48
    
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

---
+++ src/test/java/org/jboss/tools/intellij/openshift/actions/component/OpenInBrowserActionTest.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ * Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.intellij.openshift.actions.component;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import org.jboss.tools.intellij.openshift.actions.ActionTest;
+
+public class OpenInBrowserActionTest extends ActionTest {
+  @Override
+  public AnAction getAction() {
+    return new OpenInBrowserAction();
+  }
+
+  @Override
+  protected void verifyComponent(boolean visible) {
+    assertTrue(visible);
+  }
+
+  @Override
+  protected void verifyURL(boolean visible) {
+    assertTrue(visible);
+  }
+}

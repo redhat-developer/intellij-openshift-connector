bd6d7d446f51ba74872dad1a942af021675c2d9f
Stephane Bouchet <sbouchet@redhat.com>
2019-11-12 17:21:58 +0100

    used ExecHelper.submit make action visible only for java and nodejs components
    Signed-off-by: Stephane Bouchet <sbouchet@redhat.com>

--- src/main/java/org/jboss/tools/intellij/openshift/actions/component/DebugComponentAction.java
+++ src/main/java/org/jboss/tools/intellij/openshift/actions/component/DebugComponentAction.java
@@ -15,15 +15,14 @@ import com.intellij.openapi.ui.Messages;
 import org.jboss.tools.intellij.openshift.actions.OdoAction;
 import org.jboss.tools.intellij.openshift.tree.LazyMutableTreeNode;
 import org.jboss.tools.intellij.openshift.tree.application.ApplicationNode;
+import org.jboss.tools.intellij.openshift.tree.application.ApplicationsRootNode;
 import org.jboss.tools.intellij.openshift.tree.application.ComponentNode;
+import org.jboss.tools.intellij.openshift.utils.ExecHelper;
 import org.jboss.tools.intellij.openshift.utils.UIHelper;
-import org.jboss.tools.intellij.openshift.utils.odo.Component;
-import org.jboss.tools.intellij.openshift.utils.odo.ComponentState;
-import org.jboss.tools.intellij.openshift.utils.odo.Odo;
+import org.jboss.tools.intellij.openshift.utils.odo.*;
 
 import javax.swing.tree.TreePath;
 import java.io.IOException;
-import java.util.concurrent.CompletableFuture;
 
 public class DebugComponentAction extends OdoAction {
     public DebugComponentAction() {
@@ -32,11 +31,32 @@ public class DebugComponentAction extends OdoAction {
 
     @Override
     public boolean isVisible(Object selected) {
+        //TODO make invisible when already debugging a component to the same port
         boolean visible = super.isVisible(selected);
         if (visible) {
-            visible = ((Component) ((ComponentNode) selected).getUserObject()).getState() == ComponentState.PUSHED;
+            ComponentNode componentNode = (ComponentNode) selected;
+            Component component = (Component) componentNode.getUserObject();
+            return (isPushed(component) && isDebuggable(componentNode, component));
         }
-        return visible;
+        return false;
+    }
+
+    private boolean isDebuggable(ComponentNode componentNode, Component component) {
+        ApplicationNode applicationNode = (ApplicationNode) componentNode.getParent();
+        try {
+            Odo odo = ((ApplicationsRootNode) componentNode.getRoot()).getOdo();
+            LazyMutableTreeNode projectNode = (LazyMutableTreeNode) applicationNode.getParent();
+            ApplicationsRootNode root = (ApplicationsRootNode) applicationNode.getRoot();
+            ComponentInfo info = odo.getComponentInfo(root.getClient(), projectNode.toString(), applicationNode.toString(), component.getName());
+            return info.getComponentTypeName().equals("java") || info.getComponentTypeName().equals("nodejs");
+        } catch (IOException e) {
+            UIHelper.executeInUI(() -> Messages.showErrorDialog("Error: " + e.getLocalizedMessage(), "Debug"));
+        }
+        return false;
+    }
+
+    private boolean isPushed(Component component) {
+        return component.getState() == ComponentState.PUSHED;
     }
 
     @Override
@@ -45,7 +65,7 @@ public class DebugComponentAction extends OdoAction {
         Component component = (Component) componentNode.getUserObject();
         ApplicationNode applicationNode = (ApplicationNode) componentNode.getParent();
         LazyMutableTreeNode projectNode = (LazyMutableTreeNode) applicationNode.getParent();
-        CompletableFuture.runAsync(() -> {
+        ExecHelper.submit(() -> {
             try {
                 odo.debug(projectNode.toString(), applicationNode.toString(), component.getPath(), component.getName(), null);
             } catch (IOException e) {

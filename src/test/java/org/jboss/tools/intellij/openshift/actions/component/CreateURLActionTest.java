3a9a7d061213d944207a054a389f874ae4b311db
Stephane Bouchet <sbouchet@redhat.com>
2020-02-05 10:03:32 +0100

    issue 141 : cleanup
    Signed-off-by: Stephane Bouchet <sbouchet@redhat.com>

--- src/test/java/org/jboss/tools/intellij/openshift/actions/component/CreateURLActionTest.java
+++ src/test/java/org/jboss/tools/intellij/openshift/actions/component/CreateURLActionTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019 Red Hat, Inc.
+ * Copyright (c) 2020 Red Hat, Inc.
  * Distributed under license by Red Hat, Inc. All rights reserved.
  * This program is made available under the terms of the
  * Eclipse Public License v2.0 which accompanies this distribution,
@@ -14,13 +14,13 @@ import com.intellij.openapi.actionSystem.AnAction;
 import org.jboss.tools.intellij.openshift.actions.ActionTest;
 
 public class CreateURLActionTest extends ActionTest {
-  @Override
-  public AnAction getAction() {
-    return new CreateURLAction();
-  }
+    @Override
+    public AnAction getAction() {
+        return new CreateURLAction();
+    }
 
-  @Override
-  protected void verifyPushedComponent(boolean visible) {
-    assertTrue(visible);
-  }
+    @Override
+    protected void verifyPushedComponent(boolean visible) {
+        assertTrue(visible);
+    }
 }
--- src/test/java/org/jboss/tools/intellij/openshift/actions/component/DeleteComponentActionTest.java
+++ src/test/java/org/jboss/tools/intellij/openshift/actions/component/DeleteComponentActionTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019 Red Hat, Inc.
+ * Copyright (c) 2020 Red Hat, Inc.
  * Distributed under license by Red Hat, Inc. All rights reserved.
  * This program is made available under the terms of the
  * Eclipse Public License v2.0 which accompanies this distribution,
@@ -15,24 +15,24 @@ import org.jboss.tools.intellij.openshift.actions.ActionTest;
 import org.jboss.tools.intellij.openshift.actions.application.DeleteApplicationAction;
 
 public class DeleteComponentActionTest extends ActionTest {
-  @Override
-  public AnAction getAction() {
-    return new DeleteComponentAction();
-  }
+    @Override
+    public AnAction getAction() {
+        return new DeleteComponentAction();
+    }
 
-  @Override
-  protected void verifyPushedComponent(boolean visible)  {
-    assertTrue(visible);
-  }
+    @Override
+    protected void verifyPushedComponent(boolean visible) {
+        assertTrue(visible);
+    }
 
-  @Override
-  protected void verifyNotPushedComponent(boolean visible)  {
-    assertTrue(visible);
-  }
+    @Override
+    protected void verifyNotPushedComponent(boolean visible) {
+        assertTrue(visible);
+    }
 
-  @Override
-  protected void verifyNoContextComponent(boolean visible)  {
-    assertTrue(visible);
-  }
+    @Override
+    protected void verifyNoContextComponent(boolean visible) {
+        assertTrue(visible);
+    }
 
 }

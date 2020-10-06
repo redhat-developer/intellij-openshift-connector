56ed6aabe622fcd0330c0113338f5c38357c2b23
Stephane Bouchet <sbouchet@redhat.com>
2020-10-06 12:38:00 +0200

    #39: implement login from oc commandline from clipboard
    Signed-off-by: Stephane Bouchet <sbouchet@redhat.com>

---
+++ src/main/java/org/jboss/tools/intellij/openshift/utils/OCCommandUtils.java
@@ -0,0 +1,122 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.intellij.openshift.utils;
+
+import java.util.regex.Pattern;
+
+
+/**
+ * Util class for parsing oc command.
+ *
+ * @author Josef Kopriva
+ */
+public class OCCommandUtils {
+
+    /**
+     * Validates is oc command is in correct format with required fields.
+     *
+     * @param ocCommand
+     * @return false command has not correct format
+     */
+    public static boolean isValidCommand(String ocCommand) {
+        ocCommand = ocCommand.trim();
+        return (ocCommand.startsWith("oc login ") && isValidAuthMethod(ocCommand)
+                && getServer(ocCommand) != null);
+    }
+
+    /**
+     * Returns true if authorization schema of oc command is valid (basic/OAuth).
+     *
+     * @param ocCommand
+     * @return false command has not correct authorization schema
+     */
+    public static boolean isValidAuthMethod(String ocCommand) {
+        ocCommand = ocCommand.trim();
+        return (ocCommand.contains(" -u") || ocCommand.contains(" --username") || ocCommand.contains("--token="));
+    }
+
+    /**
+     * Parses server address from oc command.
+     *
+     * @param ocCommand
+     * @return server address
+     */
+    public static String getServer(String ocCommand) {
+        String server = applyPattern(ocCommand, "(?<=\\s)https[a-zA-Z0-9:/.-]+", 0);
+        if (server != null) {
+            return server;
+        } else {
+            return applyPattern(ocCommand, "(?<=[\\s=])https[a-zA-Z0-9:/.-]+", 0);
+        }
+    }
+
+    /**
+     * Parses token from oc command.
+     *
+     * @param ocCommand
+     * @return token
+     */
+    public static String getToken(String ocCommand) {
+        return applyPattern(ocCommand, "(?<=--token=)[a-zA-Z0-9:._-]+", 0);
+    }
+
+    /**
+     * Parses username from oc command.
+     *
+     * @param ocCommand
+     * @return username
+     */
+    public static String getUsername(String ocCommand) {
+        String username = applyPattern(ocCommand, "(?<=-u[\\s=])[a-zA-Z0-9:]+", 0);
+        if (username != null) {
+            return username;
+        } else {
+            return applyPattern(ocCommand, "(?<=--username[\\s=])[a-zA-Z0-9:]+", 0);
+        }
+    }
+
+    /**
+     * Parses password from oc command.
+     *
+     * @param ocCommand
+     * @return password
+     */
+    public static String getPassword(String ocCommand) {
+        String password = searchInStringForPattern(ocCommand, "(?<=-p[\\s=])(.*)(?=\\b)");
+        if (password != null) {
+            return password;
+        } else {
+            return searchInStringForPattern(ocCommand, "(?<=--password[\\s=])(.*)(?=\\b)");
+        }
+    }
+
+    private static String searchInStringForPattern(String stringToVerify, String pattern) {
+        if (stringToVerify.contains("-p")) {
+            return applyPattern(stringToVerify, pattern);
+        }
+        return null;
+    }
+
+    private static String applyPattern(String stringToVerify, String pattern) {
+        return applyPattern(stringToVerify, pattern, 1);
+    }
+
+    private static String applyPattern(String stringToVerify, String pattern, int group) {
+        stringToVerify = stringToVerify.trim();
+        Pattern patternToken = Pattern.compile(pattern);
+        java.util.regex.Matcher matcherToken = patternToken.matcher(stringToVerify);
+        if (matcherToken.find()) {
+            return matcherToken.group(group);
+        }
+        return null;
+    }
+
+}
---
+++ src/test/java/org/jboss/tools/intellij/openshift/utils/OCCommandUtilsTest.java
@@ -0,0 +1,96 @@
+/*************************************************************************************
+ * Copyright (c) 2020 Red Hat, Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     JBoss by Red Hat - Initial implementation.
+ ************************************************************************************/
+package org.jboss.tools.intellij.openshift.utils;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit tests for OCCommandUtils for testing correct parsing of oc login command
+ * @author Josef Kopriva
+ */
+public class OCCommandUtilsTest {
+    @Test
+    public void testCommandWithToken() {
+        assertEquals("123456789123456789",
+                OCCommandUtils.getToken("oc login https://api.engint.openshift.com --token=123456789123456789"));
+        assertEquals("123456789123456789",
+                OCCommandUtils.getToken("oc login https://api.-uengint.openshift.com --token=123456789123456789   "));
+        assertEquals("1234567891234567..89",
+                OCCommandUtils.getToken("oc login https://api.-uengint.openshift.com --token=1234567891234567..89   "));
+        assertEquals("12345678912345678_9",
+                OCCommandUtils.getToken("oc login https://api.-uengint.openshift.com --token=12345678912345678_9   "));
+        assertEquals("1234567891234567.89",
+                OCCommandUtils.getToken("oc login https://api.-uengint.openshift.com --token=1234567891234567.89   "));
+        assertEquals("1234567891234567-89",
+                OCCommandUtils.getToken("oc login https://api.-uengint.openshift.com --token=1234567891234567-89   "));
+    }
+
+    @Test
+    public void testCommandWithPassword() {
+        assertEquals("developerPassword", OCCommandUtils.getPassword("oc login -u developer -p developerPassword"));
+        assertEquals("developerPassword", OCCommandUtils.getPassword("oc login -u developer -p developerPassword"));
+        assertEquals("developerPassword", OCCommandUtils.getPassword("oc login -u developer --password developerPassword"));
+        assertEquals("developerPassword", OCCommandUtils.getPassword("oc login -u developer --password=developerPassword"));
+    }
+
+    @Test
+    public void testValidCommand() {
+        assertFalse(
+                OCCommandUtils.isValidCommand("oc loginhttps://api.engint.openshift.com --token=123456789123456789"));
+        assertTrue(
+                OCCommandUtils.isValidCommand("oc login https://api.engint.openshift.com --token=123456789123456789"));
+        assertTrue(OCCommandUtils.isValidCommand("oc login https://12.34.5.6:8443 -u developer -p deve"));
+    }
+
+    @Test
+    public void testServerAddress() {
+        assertEquals("https://api.engint.openshift.com",
+                OCCommandUtils.getServer("oc login https://api.engint.openshift.com --token=123456789123456789"));
+        assertEquals("https://api.engint.openshift.com",
+                OCCommandUtils.getServer("oc login -s=https://api.engint.openshift.com --token=123456789123456789"));
+        assertEquals("https://api.engint.openshift.com",
+                OCCommandUtils.getServer("oc login -s https://api.engint.openshift.com --token=123456789123456789"));
+        assertEquals("https://api.engint.openshift.com",
+                OCCommandUtils.getServer("oc login --server=https://api.engint.openshift.com --token=123456789123456789"));
+        assertEquals("https://api.engint.openshift.com",
+                OCCommandUtils.getServer("oc login --server https://api.engint.openshift.com --token=123456789123456789"));
+    }
+
+    @Test
+    public void testUsername() {
+        assertEquals("developer", OCCommandUtils.getUsername("oc login -u developer -p developerPassword"));
+        assertEquals("developer", OCCommandUtils.getUsername("oc login -u=developer -p developerPassword"));
+        assertEquals("developer", OCCommandUtils.getUsername("oc login --username developer -p developerPassword"));
+        assertEquals("developer", OCCommandUtils.getUsername("oc login --username=developer -p developerPassword"));
+    }
+
+    @Test
+    public void testCommandValidOnlyUsername() {
+        String command = "oc login -u system:admin -n default https://api.starter-us-west-1.openshift.com";
+        assertTrue(OCCommandUtils.isValidCommand(command));
+        assertEquals("system:admin", OCCommandUtils.getUsername(command));
+        assertEquals("https://api.starter-us-west-1.openshift.com",OCCommandUtils.getServer(command));
+    }
+
+    @Test
+    public void testCommandValidDifferentOrder() {
+        String command = "oc login -u system:admin -n default https://api.starter-us-west-1.openshift.com -p 123456";
+        assertTrue(OCCommandUtils.isValidCommand(command));
+        assertEquals("system:admin", OCCommandUtils.getUsername(command));
+        assertEquals("123456", OCCommandUtils.getPassword(command));
+        assertEquals("https://api.starter-us-west-1.openshift.com",OCCommandUtils.getServer(command));
+    }
+
+}

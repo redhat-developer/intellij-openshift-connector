91b091b071aafeed0998568593d2767a54c29ce4
Jeff MAURY <jmaury@redhat.com>
2021-03-26 14:30:08 +0100

    fix: Paste credentials from OCP4 does not work
    Fixes #273
    
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- src/main/java/org/jboss/tools/intellij/openshift/utils/OCCommandUtils.java
+++ src/main/java/org/jboss/tools/intellij/openshift/utils/OCCommandUtils.java
@@ -65,7 +65,7 @@ public class OCCommandUtils {
      * @return token
      */
     public static String getToken(String ocCommand) {
-        return applyPattern(ocCommand, "(?<=--token=)[a-zA-Z0-9:._-]+", 0);
+        return applyPattern(ocCommand, "(?<=--token=)[~a-zA-Z0-9:._-]+", 0);
     }
 
     /**

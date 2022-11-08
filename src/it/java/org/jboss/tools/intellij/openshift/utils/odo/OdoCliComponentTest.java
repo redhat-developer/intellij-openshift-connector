c6a5020aa16354b539c9537fc175e60d37f2db1d
Jeff MAURY <jmaury@redhat.com>
2022-11-08 12:12:35 +0100

    chore(skip-release): Prepare for 1.0.0 (#464)
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- src/it/java/org/jboss/tools/intellij/openshift/utils/odo/OdoCliComponentTest.java
+++ src/it/java/org/jboss/tools/intellij/openshift/utils/odo/OdoCliComponentTest.java
@@ -122,7 +122,7 @@ public class OdoCliComponentTest extends OdoCliTest {
         try {
             createComponent(project, component, feature);
             List<URL> urls = odo.listURLs(project, COMPONENT_PATH, component);
-            assertEquals(1, urls.size());
+            assertEquals(2, urls.size());
         } finally {
             odo.deleteProject(project);
         }

bb1168915909272eebf57678627a525df8f36fbf
Andre Dietisheim <adietish@redhat.com>
2022-11-17 18:55:51 +0100

    chore: replaced disposable (was: project) & removed reflective buildModel (#475)
    Signed-off-by: Andre Dietisheim <adietish@redhat.com>

--- src/main/java/org/jboss/tools/intellij/openshift/WindowToolFactory.java
+++ src/main/java/org/jboss/tools/intellij/openshift/WindowToolFactory.java
@@ -10,9 +10,7 @@
  ******************************************************************************/
 package org.jboss.tools.intellij.openshift;
 
-import com.intellij.ide.util.treeView.AbstractTreeStructure;
 import com.intellij.ide.util.treeView.NodeRenderer;
-import com.intellij.openapi.Disposable;
 import com.intellij.openapi.actionSystem.ActionPlaces;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
@@ -20,6 +18,7 @@ import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.ui.PopupHandler;
 import com.intellij.ui.components.JBPanel;
 import com.intellij.ui.components.JBScrollPane;
+import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
 import com.intellij.ui.tree.AsyncTreeModel;
 import com.intellij.ui.tree.StructureTreeModel;
@@ -30,52 +29,26 @@ import org.jboss.tools.intellij.openshift.tree.application.ApplicationsTreeStruc
 import org.jetbrains.annotations.NotNull;
 
 import java.awt.BorderLayout;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 
 
 public class WindowToolFactory implements ToolWindowFactory {
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
-        try {
-            ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
+        ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
 
-            JBPanel<JBPanel> panel = new JBPanel<>();
-            panel.setLayout(new BorderLayout());
-            ApplicationsTreeStructure structure = new ApplicationsTreeStructure(project);
-            StructureTreeModel<ApplicationsTreeStructure> model = buildModel(structure, project);
-            new MutableModelSynchronizer<>(model, structure, structure);
-            Tree tree = new Tree(new AsyncTreeModel(model, project));
-            tree.putClientProperty(Constants.STRUCTURE_PROPERTY, structure);
-            tree.setCellRenderer(new NodeRenderer());
-            tree.setRootVisible(false);
-            PopupHandler.installPopupHandler(tree, "org.jboss.tools.intellij.tree", ActionPlaces.UNKNOWN);
-            panel.add(new JBScrollPane(tree), BorderLayout.CENTER);
-            toolWindow.getContentManager().addContent(contentFactory.createContent(panel, "", false));
-            TreeHelper.addLinkSupport(tree);
-        } catch (IllegalAccessException | InvocationTargetException | InstantiationException | NoSuchMethodException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    /**
-     * Build the model through reflection as StructureTreeModel does not have a stable API.
-     *
-     * @param structure the structure to associate
-     * @param project the IJ project
-     * @return the build model
-     * @throws IllegalAccessException
-     * @throws InvocationTargetException
-     * @throws InstantiationException
-     * @throws NoSuchMethodException
-     */
-    private StructureTreeModel buildModel(ApplicationsTreeStructure structure, Project project) throws IllegalAccessException, InvocationTargetException, InstantiationException, NoSuchMethodException {
-        try {
-            Constructor<StructureTreeModel> constructor = StructureTreeModel.class.getConstructor(new Class[] {AbstractTreeStructure.class});
-            return constructor.newInstance(structure);
-        } catch (NoSuchMethodException e) {
-            Constructor<StructureTreeModel> constructor = StructureTreeModel.class.getConstructor(new Class[] {AbstractTreeStructure.class, Disposable.class});
-            return constructor.newInstance(structure, project);
-        }
+        JBPanel<JBPanel> panel = new JBPanel<>();
+        panel.setLayout(new BorderLayout());
+        Content content = contentFactory.createContent(panel, "", false);
+        ApplicationsTreeStructure structure = new ApplicationsTreeStructure(project);
+        StructureTreeModel<ApplicationsTreeStructure> model = new StructureTreeModel<>(structure, content);
+        new MutableModelSynchronizer<>(model, structure, structure);
+        Tree tree = new Tree(new AsyncTreeModel(model, content));
+        tree.putClientProperty(Constants.STRUCTURE_PROPERTY, structure);
+        tree.setCellRenderer(new NodeRenderer());
+        tree.setRootVisible(false);
+        PopupHandler.installPopupHandler(tree, "org.jboss.tools.intellij.tree", ActionPlaces.UNKNOWN);
+        panel.add(new JBScrollPane(tree), BorderLayout.CENTER);
+        toolWindow.getContentManager().addContent(content);
+        TreeHelper.addLinkSupport(tree);
     }
 }

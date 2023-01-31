355cb72c86c5f549320829cf6dfb9789baed7544
Stephane Bouchet <sbouchet@redhat.com>
2023-01-31 14:48:20 +0100

    update postgresql operator version
    Signed-off-by: Stephane Bouchet <sbouchet@redhat.com>

--- src/it/java/org/jboss/tools/intellij/openshift/utils/odo/OdoCliTest.java
+++ src/it/java/org/jboss/tools/intellij/openshift/utils/odo/OdoCliTest.java
@@ -32,7 +32,7 @@ import static org.junit.Assert.assertNotNull;
 public abstract class OdoCliTest extends BaseTest {
 
     public static final String COMPONENT_PATH = "src/it/projects/springboot-rest";
-    public static final String SERVICE_TEMPLATE = "cloud-native-postgresql.v1.17.2";
+    public static final String SERVICE_TEMPLATE = "cloud-native-postgresql.v1.17.3";
     public static final String SERVICE_CRD = "clusters.postgresql.k8s.enterprisedb.io";
     public static final String REGISTRY_URL = "https://registry.stage.devfile.io";
     public static final String REGISTRY_NAME = "RegistryForITTests";

8fa2ce5303d1fcdef8360a370666070d148fcea0
Stephane Bouchet <sbouchet@redhat.com>
2023-06-13 16:29:20 +0000

    fix Integration tests
    Signed-off-by: Stephane Bouchet <sbouchet@redhat.com>

--- src/it/java/org/jboss/tools/intellij/openshift/utils/odo/OdoCliFactoryTest.java
+++ src/it/java/org/jboss/tools/intellij/openshift/utils/odo/OdoCliFactoryTest.java
@@ -10,17 +10,17 @@
  ******************************************************************************/
 package org.jboss.tools.intellij.openshift.utils.odo;
 
-import org.jboss.tools.intellij.openshift.BaseTest;
+import com.intellij.testFramework.fixtures.BasePlatformTestCase;
 import org.junit.Test;
 
 import java.util.concurrent.ExecutionException;
 
 import static org.junit.Assert.assertNotNull;
 
-public class OdoCliFactoryTest extends BaseTest {
+public class OdoCliFactoryTest extends BasePlatformTestCase {
     @Test
     public void getOdo() throws ExecutionException, InterruptedException {
-        Odo odo = OdoCliFactory.getInstance().getOdo(project).get();
+        Odo odo = OdoCliFactory.getInstance().getOdo(getProject()).get();
         assertNotNull(odo);
     }
 }

5653aefcdeae054f51022bf43d44abed8cac6868
Richard Kocian <rkocian@redhat.com>
2023-06-29 10:08:11 +0200

    fix: correct xpath for selecting new project
    Signed-off-by: Richard Kocian <rkocian@redhat.com>

--- src/it/java/org/jboss/tools/intellij/openshift/test/ui/utils/ProjectUtility.java
+++ src/it/java/org/jboss/tools/intellij/openshift/test/ui/utils/ProjectUtility.java
@@ -55,7 +55,7 @@ public class ProjectUtility {
     }
 
     public static void selectNewProjectType(RemoteRobot robot, String projectType) {
-        ComponentFixture newProjectTypeList = robot.findAll(ComponentFixture.class, byXpath("JBList", "//div[@class='JBList']")).get(0);
+        ComponentFixture newProjectTypeList = robot.findAll(ComponentFixture.class, byXpath("JBList", "//div[contains(@visible_text, 'FX')]")).get(0);
         newProjectTypeList.findText(projectType).click();
     }
 

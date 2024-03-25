75c3121e0b800c5a2608817ebf248bbaaab282a9
Martin Szuc <mszuc@redhat.com>
2024-03-25 23:45:16 +0100

    feat(UItest): right click and select on Openshift tree item
    Signed-off-by: Martin Szuc <mszuc@redhat.com>

--- src/it/java/org/jboss/tools/intellij/openshift/test/ui/views/OpenshiftView.java
+++ src/it/java/org/jboss/tools/intellij/openshift/test/ui/views/OpenshiftView.java
@@ -12,10 +12,7 @@ package org.jboss.tools.intellij.openshift.test.ui.views;
 
 import com.intellij.remoterobot.RemoteRobot;
 import com.intellij.remoterobot.data.RemoteComponent;
-import com.intellij.remoterobot.fixtures.ContainerFixture;
-import com.intellij.remoterobot.fixtures.DefaultXpath;
-import com.intellij.remoterobot.fixtures.FixtureName;
-import com.intellij.remoterobot.fixtures.JTreeFixture;
+import com.intellij.remoterobot.fixtures.*;
 import com.intellij.remoterobot.utils.WaitForConditionTimeoutException;
 import com.redhat.devtools.intellij.commonuitest.fixtures.mainidewindow.toolwindowspane.ToolWindowPane;
 import org.jetbrains.annotations.NotNull;
@@ -24,6 +21,7 @@ import java.time.Duration;
 
 import static com.intellij.remoterobot.search.locators.Locators.byXpath;
 import static com.intellij.remoterobot.utils.RepeatUtilsKt.waitFor;
+import static com.redhat.devtools.intellij.commonuitest.utils.steps.SharedSteps.waitForComponentByXpath;
 
 /**
  * 
@@ -33,43 +31,50 @@ import static com.intellij.remoterobot.utils.RepeatUtilsKt.waitFor;
 @DefaultXpath(by = "OpenshiftView type", xpath = "//div[@class='IdeFrameImpl']")
 @FixtureName(name = "Openshift View")
 public class OpenshiftView extends ContainerFixture {
-  public OpenshiftView(@NotNull RemoteRobot remoteRobot, @NotNull RemoteComponent remoteComponent) {
-    super(remoteRobot, remoteComponent);
-  }
-
-  public void openView() {
-    final ToolWindowPane toolWindowPane = find(ToolWindowPane.class);
-    waitFor(Duration.ofSeconds(10), Duration.ofSeconds(1), "The 'OpenShift' stripe button is not available.", () -> isStripeButtonAvailable(toolWindowPane, "OpenShift"));
-    toolWindowPane.stripeButton("OpenShift", false).click();
-  }
-
-  public void closeView() {
-    final ToolWindowPane toolWindowPane = find(ToolWindowPane.class);
-    toolWindowPane.button(byXpath("//div[@tooltiptext='OpenShift']"), Duration.ofSeconds(2)).click();
-  }
-
-  public void expandOpenshiftViewTree(String path) {
-      getOpenshiftConnectorTree().expand(path);
-  }
-
-  public void waitForTreeItem(String itemText, int duration, int interval) {
-    waitFor(Duration.ofSeconds(duration),
-        Duration.ofSeconds(interval),
-        "The " + itemText + " project is still not available.",
-        () -> getOpenshiftConnectorTree().hasText(itemText));
-  }
-
-  public JTreeFixture getOpenshiftConnectorTree(){
-    return find(JTreeFixture.class, byXpath("//div[@class='Tree']"), Duration.ofSeconds(30));
-  }
-
-  private boolean isStripeButtonAvailable(ToolWindowPane toolWindowPane, String label) {
-    try {
-      toolWindowPane.stripeButton(label, false);
-    } catch (WaitForConditionTimeoutException e) {
-      return false;
+    public OpenshiftView(@NotNull RemoteRobot remoteRobot, @NotNull RemoteComponent remoteComponent) {
+        super(remoteRobot, remoteComponent);
     }
-    return true;
-  }
+
+    public void openView() {
+        final ToolWindowPane toolWindowPane = find(ToolWindowPane.class);
+        waitFor(Duration.ofSeconds(10), Duration.ofSeconds(1), "The 'OpenShift' stripe button is not available.", () -> isStripeButtonAvailable(toolWindowPane, "OpenShift"));
+        toolWindowPane.stripeButton("OpenShift", false).click();
+    }
+
+    public void closeView() {
+        final ToolWindowPane toolWindowPane = find(ToolWindowPane.class);
+        toolWindowPane.button(byXpath("//div[@tooltiptext='OpenShift']"), Duration.ofSeconds(2)).click();
+    }
+
+    public void expandOpenshiftViewTree(String path) {
+        getOpenshiftConnectorTree().expand(path);
+    }
+
+    public void waitForTreeItem(String itemText, int duration, int interval) {
+        waitFor(Duration.ofSeconds(duration),
+                Duration.ofSeconds(interval),
+                "The " + itemText + " project is still not available.",
+                () -> getOpenshiftConnectorTree().hasText(itemText));
+    }
+
+    public JTreeFixture getOpenshiftConnectorTree() {
+        return find(JTreeFixture.class, byXpath("//div[@class='Tree']"), Duration.ofSeconds(30));
+    }
+
+    private boolean isStripeButtonAvailable(ToolWindowPane toolWindowPane, String label) {
+        try {
+            toolWindowPane.stripeButton(label, false);
+        } catch (WaitForConditionTimeoutException e) {
+            return false;
+        }
+        return true;
+    }
+
+    public void menuRightClickAndSelect(RemoteRobot robot, int row, String selection) {
+        getOpenshiftConnectorTree().clickRow(row);
+        getOpenshiftConnectorTree().rightClickRow(row);
+        waitForComponentByXpath(robot, 5, 1, byXpath("//div[@text='" + selection + "']"));
+        robot.find(ComponentFixture.class, byXpath("//div[@text='" + selection + "']")).click();
+    }
 
 }

fb7f5780fa0dd65acaf9a9202256588b7a8f3599
Stephane Bouchet <sbouchet@redhat.com>
2024-03-28 10:25:07 +0100

    build(deps): bump io.jsonwebtoken:jjwt-impl from 0.11.2 to 0.12.5
    fix build
    
    Signed-off-by: Stephane Bouchet <sbouchet@redhat.com>

--- src/test/java/org/jboss/tools/intellij/openshift/oauth/OAuthUtilsTest.java
+++ src/test/java/org/jboss/tools/intellij/openshift/oauth/OAuthUtilsTest.java
@@ -1,12 +1,12 @@
 /*************************************************************************************
- * Copyright (c) 2020 Red Hat, Inc. and others.
+ * Copyright (c) 2024 Red Hat, Inc. and others.
  * Distributed under license by Red Hat, Inc. All rights reserved.
  * This program is made available under the terms of the
  * Eclipse Public License v2.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v20.html
  *
  * Contributors:
- *     JBoss by Red Hat - Initial implementation.
+ *     Red Hat, Inc. - initial API and implementation
  ************************************************************************************/
 package org.jboss.tools.intellij.openshift.oauth;
 

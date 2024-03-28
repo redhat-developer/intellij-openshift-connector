6288c41d8299813b620744f7f4202729f446c054
Stephane Bouchet <sbouchet@redhat.com>
2024-03-28 10:08:24 +0100

    build(deps): bump io.jsonwebtoken:jjwt-impl from 0.11.2 to 0.12.5
    fix build
    
    Signed-off-by: Stephane Bouchet <sbouchet@redhat.com>

--- src/test/java/org/jboss/tools/intellij/openshift/oauth/OAuthUtilsTest.java
+++ src/test/java/org/jboss/tools/intellij/openshift/oauth/OAuthUtilsTest.java
@@ -14,39 +14,55 @@ import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.UnsupportedJwtException;
 import org.jboss.tools.intellij.openshift.oauth.model.IAuthorizationServer;
 import org.junit.Test;
+import org.keycloak.adapters.KeycloakDeployment;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class OAuthUtilsTest {
-  @Test
-  public void testDecodeEmailFromToken() {
-    IAuthorizationServer server = mock(IAuthorizationServer.class);
-    assertThrows(IllegalArgumentException.class, () ->
-      OAuthUtils.decodeEmailFromToken(server, ""));
-    assertThrows(MalformedJwtException.class, () ->
-      OAuthUtils.decodeEmailFromToken(server, "1234"));
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testDecodeEmailFromTokenNoToken() {
+    OAuthUtils.decodeEmailFromToken(mock(IAuthorizationServer.class), "");
+    fail("should not decode empty string");
+  }
+
+  @Test(expected = MalformedJwtException.class)
+  public void testDecodeEmailFromTokenBadToken() {
+    OAuthUtils.decodeEmailFromToken(mock(IAuthorizationServer.class), "1234");
+    fail("should not decode bad token");
+  }
+
+  @Test(expected = UnsupportedJwtException.class)
+  public void testDecodeEmailFromTokenNoneAlgHeader() {
     String noneAlgHeader = Base64.getEncoder().encodeToString("{\"alg\":\"none\"}".getBytes(StandardCharsets.UTF_8));
     String payload = Base64.getEncoder().encodeToString("payload".getBytes(StandardCharsets.UTF_8));
-    assertThrows(UnsupportedJwtException.class, () ->
-      OAuthUtils.decodeEmailFromToken(server, noneAlgHeader + '.' + payload + '.'));
+    OAuthUtils.decodeEmailFromToken(mock(IAuthorizationServer.class), noneAlgHeader + '.' + payload + '.');
+    fail("should not decode None Alg header token");
+  }
+
+  @Test(expected = MalformedJwtException.class)
+  public void testDecodeEmailFromTokenWrongAlgHeader() {
     String wrongAlgHeader = Base64.getEncoder().encodeToString("{\"alg\":\"HS256\"}".getBytes(StandardCharsets.UTF_8));
-    assertThrows(MalformedJwtException.class, () ->
-      OAuthUtils.decodeEmailFromToken(server, wrongAlgHeader + '.' + payload + '.'));
+    String payload = Base64.getEncoder().encodeToString("payload".getBytes(StandardCharsets.UTF_8));
+    OAuthUtils.decodeEmailFromToken(mock(IAuthorizationServer.class), wrongAlgHeader + '.' + payload + '.');
+    fail("should not decode None Alg header token");
   }
 
   @Test
   public void testGetDeployment() {
     IAuthorizationServer server = mock(IAuthorizationServer.class);
-    when(server.getRealm()).thenReturn("");
-    when(server.getClientId()).thenReturn("");
-    when(server.getURL()).thenReturn("");
-    assertNotNull(OAuthUtils.getDeployment(server));
+    when(server.getRealm()).thenReturn("redhat-external");
+    when(server.getClientId()).thenReturn("vscode-redhat-account");
+    when(server.getURL()).thenReturn("https://sso.redhat.com/auth");
+    KeycloakDeployment deployment = OAuthUtils.getDeployment(server);
+    assertNotNull(deployment);
+    assertNotNull(deployment.getPublicKeyLocator());
   }
 
 }

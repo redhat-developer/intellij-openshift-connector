35284337ba87a821f00a0a7c3ee431505d975da2
Martin Szuc <mszuc@redhat.com>
2024-03-28 01:49:41 +0100

    feat(UItest): Cluster test classes ->
    Login with username test
    About test case when logged in
    
    refactored: removed Unused BaseUITest.java since tests have been moved elsewhere
    
    Signed-off-by: Martin Szuc <mszuc@redhat.com>

---
+++ src/it/java/org/jboss/tools/intellij/openshift/test/ui/tests_cluster/LoginClusterTest.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2024 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors: Martin Szuc
+ * Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.intellij.openshift.test.ui.tests_cluster;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Martin Szuc
+ * Test class verifying functionality of Log in to Cluster
+ */
+public class LoginClusterTest extends AbstractClusterTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoginClusterTest.class);
+
+    @Test
+    public void usernameLoginTest() {
+        LOGGER.info("usernameLoginTest: Start");
+        logOut();
+        loginWithUsername();
+        verifyClusterLogin(currentClusterUrl);
+        logOut();
+        LOGGER.info("usernameLoginTest: End");
+    }
+}

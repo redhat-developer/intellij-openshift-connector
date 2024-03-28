4d1d9ddebeb06d2e73d0c0a30ff4552d4307e1c2
Martin Szuc <mszuc@redhat.com>
2024-03-28 01:49:41 +0100

    feat(UItest): Cluster test classes ->
    Login with username test
    About test case when logged in
    
    refactored: removed Unused BaseUITest.java since tests have been moved elsewhere
    
    Signed-off-by: Martin Szuc <mszuc@redhat.com>

---
+++ src/it/java/org/jboss/tools/intellij/openshift/test/ui/tests_cluster/AboutClusterTest.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * Copyright (c) 2024 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors: Martin Szuc
+ * Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.intellij.openshift.test.ui.tests_cluster;
+
+import com.intellij.remoterobot.fixtures.ComponentFixture;
+import org.jboss.tools.intellij.openshift.test.ui.tests_public.AboutPublicTest;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static com.intellij.remoterobot.search.locators.Locators.byXpath;
+import static com.jayway.jsonpath.internal.path.PathCompiler.fail;
+import static org.jboss.tools.intellij.openshift.test.ui.utils.constants.XPathConstants.*;
+
+/**
+ * @author Martin Szuc
+ * Test class verifying functionality of About button when right-clicked on cluster URL item in Openshift view when logged in to cluster
+ */
+public class AboutClusterTest extends AbstractClusterTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AboutClusterTest.class);
+
+    @Test
+    public void aboutLoggedInTest() {
+        LOGGER.info("aboutLoggedInTest: Start");
+        loginWithUsername();
+        try {
+            verifyClusterLogin(currentClusterUrl);
+        } catch (Exception e) {
+            LOGGER.error("AboutLoggedInTest: Failed to login!");
+            fail("AboutLoggedInTest: Failed to login!");
+        }
+
+        AboutPublicTest.selectAboutAndGetClipboardContent();
+        AboutPublicTest.verifyClipboardContent("odo version", "Server:");
+
+        // Close the "Run" tool window
+        robot.find(ComponentFixture.class, byXpath(HIDE_BUTTON))
+                .click();
+
+        LOGGER.info("aboutLoggedInTest: End");
+
+    }
+}

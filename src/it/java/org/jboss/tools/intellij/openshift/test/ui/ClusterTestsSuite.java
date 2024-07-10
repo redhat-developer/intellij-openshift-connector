39bcb2678da7b64394627c7ae11b7d1c4d80c6b4
Martin Szuc <mszuc@redhat.com>
2024-07-10 15:42:41 +0200

    feat(UItest): Added CLUSTER test cases
    AboutClusterTest verifies About button while logged in to cluster.
    CreateComponent verifies if component can be created.
    CreateService verifies if component can be created.
    ProjectCluster verifies project manipulation on cluster.
    LoginCluster (can be skipped if already logged in env variable is set to true) verifies login functionality.
    
    Signed-off-by: Martin Szuc <mszuc@redhat.com>

---
+++ src/it/java/org/jboss/tools/intellij/openshift/test/ui/ClusterTestsSuite.java
@@ -0,0 +1,30 @@
+package org.jboss.tools.intellij.openshift.test.ui;
+
+import org.jboss.tools.intellij.openshift.test.ui.tests_cluster.*;
+import org.jboss.tools.intellij.openshift.test.ui.utils.KubeConfigUtility;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.platform.suite.api.IncludeClassNamePatterns;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({
+        LoginClusterTest.class,
+        AboutClusterTest.class,
+        ProjectClusterTest.class,
+        CreateComponentTest.class,
+        CreateServiceTest.class
+})
+@IncludeClassNamePatterns({"^.*Test$"})
+public class ClusterTestsSuite {
+    @BeforeAll
+    public static void setUp() {
+        KubeConfigUtility.backupKubeConfig();
+    }
+
+    @AfterAll
+    public static void tearDown() {
+        KubeConfigUtility.restoreKubeConfig();
+    }
+}

97b26c389e3ca873e131fa24e4f39b5dcc04434c
Andre Dietisheim <adietish@redhat.com>
2024-12-03 18:40:03 +0100

    fix: bump kubernetes-client to 7.0.0 (#921)
    Signed-off-by: Andre Dietisheim <adietish@redhat.com>

--- src/main/java/org/jboss/tools/intellij/openshift/utils/ToolFactory.java
+++ src/main/java/org/jboss/tools/intellij/openshift/utils/ToolFactory.java
@@ -12,16 +12,16 @@ package org.jboss.tools.intellij.openshift.utils;
 
 import com.intellij.openapi.project.Project;
 import com.redhat.devtools.intellij.common.utils.DownloadHelper;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import java.net.URL;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
 import org.jboss.tools.intellij.openshift.utils.helm.Helm;
 import org.jboss.tools.intellij.openshift.utils.helm.HelmCli;
 import org.jboss.tools.intellij.openshift.utils.oc.Oc;
 import org.jboss.tools.intellij.openshift.utils.oc.OcCli;
 import org.jboss.tools.intellij.openshift.utils.odo.OdoCli;
 import org.jboss.tools.intellij.openshift.utils.odo.OdoDelegate;
-
-import java.net.URL;
-import java.util.concurrent.CompletableFuture;
-import java.util.function.BiFunction;
 
 public class ToolFactory {
 
@@ -36,23 +36,21 @@ public class ToolFactory {
     return INSTANCE;
   }
 
-  private final Factory<OdoDelegate> odo = new Factory<>("odo", OdoCli::new);
-  private final Factory<Helm> helm = new Factory<>("helm", (project, command) -> new HelmCli(command));
-  private final Factory<Oc> oc = new Factory<>("oc", (project, command) -> new OcCli(command));
+  private ToolFactory() {}
 
-  private ToolFactory() {
+  public CompletableFuture<Tool<OdoDelegate>> createOdo(KubernetesClient client, Project project) {
+    return new Downloadable<OdoDelegate>("odo")
+      .downloadAndCreate(commandName -> new OdoCli(commandName, project, client));
   }
 
-  public CompletableFuture<Tool<OdoDelegate>> createOdo(Project project) {
-    return odo.create(project);
+  public CompletableFuture<Tool<Helm>> createHelm() {
+    return new Downloadable<Helm>("helm")
+      .downloadAndCreate(commandName -> new HelmCli(commandName));
   }
 
-  public CompletableFuture<Tool<Helm>> createHelm(Project project) {
-    return helm.create(project);
-  }
-
-  public CompletableFuture<Tool<Oc>> createOc(Project project) {
-    return oc.create(project);
+  public CompletableFuture<Tool<Oc>> createOc(KubernetesClient client) {
+    return new Downloadable<Oc>("helm")
+      .downloadAndCreate(commandName -> new OcCli(commandName, client));
   }
 
   public static class Tool<T> {
@@ -73,31 +71,26 @@ public class ToolFactory {
     }
   }
 
-  private static class Factory<T> {
+  private static class Downloadable<T> {
 
     private final String name;
 
     private final URL url = ToolFactory.class.getResource(TOOLS_JSON);
 
-    private final BiFunction<Project, String, T> toolFactory;
-
-    private Factory(String name, BiFunction<Project, String, T> toolFactory) {
+    private Downloadable(String name) {
       this.name = name;
-      this.toolFactory = toolFactory;
     }
 
-    private CompletableFuture<Tool<T>> create(Project project) {
-      return create(name, toolFactory, project);
-    }
-
-    private CompletableFuture<Tool<T>> create(String name, BiFunction<Project, String, T> toolFactory, Project project) {
-      return DownloadHelper.getInstance().downloadIfRequiredAsync(name, url).thenApply(toolInstance -> {
-        if (toolInstance != null) {
-          return new Tool<>(toolFactory.apply(project, toolInstance.getCommand()), toolInstance.isDownloaded());
-        } else {
-          return null;
-        }
-      });
+    private CompletableFuture<Tool<T>> downloadAndCreate(Function<String, T> factory) {
+      return DownloadHelper.getInstance()
+        .downloadIfRequiredAsync(name, url)
+        .thenApply(toolInstance -> {
+          if (toolInstance != null) {
+            return new Tool<>(factory.apply(toolInstance.getCommand()), toolInstance.isDownloaded());
+          } else {
+            return null;
+          }
+        });
     }
   }
 }
